6d85d9e9a0aefd49b8566dfe97deb1151a9ad246
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-09-13 20:06:28 +0200

    Create DataReceiver.hpp

---
+++ DataReceiver.hpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#define total_count 50
+
+void readSensorData(float* temperature, float* Rateofcharge);
+float calculateMaxValue(float* sensorData);
+float calculateMinValue(float* sensorData);
+float calculateAvg(float* sensorData);
+int printData(float* sensorData,float maxValue, float minValue, float Avg);
+void displayData(float* Temperature, float* Rateofcharge);
