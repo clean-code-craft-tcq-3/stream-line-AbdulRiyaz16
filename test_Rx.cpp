b6765bc3d2326b28adf167c41ab852d3e5b1191e
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-09-13 22:10:26 +0200

    Update test_Rx.cpp

--- test_Rx.cpp
+++ test_Rx.cpp
@@ -7,8 +7,8 @@
 TEST_CASE("Tests to check whether sensor data is read from console and also to check whether correct Max, Min and Avg values of sensor parameters are printed on console") 
 {
   float Temperature[total_count] = {0};
-  float ChargeRate[total_count] = {0};
-  displayProcessedData(&Temperature[0], &RateOfCharge[0]);
+  float RateOfCharge[total_count] = {0};
+  displayData(&Temperature[0], &RateOfCharge[0]);
   float Temp[2][2] = {{10,0.1}, {26,0.15}};
   printf("-----------------------------------------------");
   printf("%f %f",Temperature[0],RateOfCharge[0]);
