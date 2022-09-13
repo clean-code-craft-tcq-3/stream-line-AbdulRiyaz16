499c41194fba93ba60643011994d08822107f0c7
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-09-13 18:26:03 +0200

    Update DataSender.h

--- DataSender.h
+++ DataSender.h
@@ -4,5 +4,4 @@
 
 void readSensorData(float* temperature, float* chargerate);
 int printSensorData(float* temperature, float* chargerate);
-int verifyConsoleData(int count);
 int communicateSensorData();
