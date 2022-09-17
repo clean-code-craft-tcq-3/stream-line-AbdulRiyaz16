77c22dc9c6f374ecb9ee52a82440a4ad1207db76
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-09-17 14:56:17 +0200

    Update DataSender.h

--- DataSender.h
+++ DataSender.h
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-#define total_count 55
+#define total_count 50
 
 void readSensorData(float* temperature, float* chargerate);
 int printSensorData(float* temperature, float* chargerate);
