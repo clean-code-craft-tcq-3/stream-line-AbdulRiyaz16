248ef5cd6b4151d5327d29f3e6c788e7248c5d3a
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-09-17 14:03:59 +0200

    Update test_Rx.cpp

--- test_Rx.cpp
+++ test_Rx.cpp
@@ -18,35 +18,3 @@ TEST_CASE("Tests to check whether sensor data is read from console and also to c
     REQUIRE(RateOfCharge[index] == Temp[index][1]);
   }
 }
-
-TEST_CASE("Check minimum, maximum and average of temperature and RateOfCharge")
-{
-  float Temperature[total_count] = {0};
-  float RateOfCharge[total_count] = {0};
-  float observedMaxValue, observedMinValue, observedSMAValue, expectedMaxValue, expectedMinValue, expectedSAvgValue;
-  expectedMaxValue = 50;
-  expectedMinValue = 0;
-  expectedSAvgValue = 18;
-  displayData(&Temperature[0], &RateOfCharge[0]);
-  observedMaxValue = calculateMaxValue(&Temperature[0]);
-  observedMinValue = calculateMinValue(&Temperature[0]);
-  observedSMAValue = calculateAvg(&Temperature[0]);
-  REQUIRE(observedMaxValue == expectedMaxValue);
-  REQUIRE(observedMinValue == expectedMinValue);
-  REQUIRE(observedSMAValue == expectedSAvgValue);
-
-  REQUIRE(printData(&Temperature[0],50,0,18) == 1);  
-
-  expectedMaxValue = 0.50;
-  expectedMinValue = 0.00;
-  expectedSAvgValue = 0.162;
-  observedMaxValue = calculateMaxValue(&RateOfCharge[0]);
-  observedMinValue = calculateMinValue(&RateOfCharge[0]);
-  observedSMAValue = calculateAvg(&RateOfCharge[0]);
-  REQUIRE(observedMaxValue == expectedMaxValue);
-  REQUIRE(observedMinValue == expectedMinValue);
-  REQUIRE(observedSMAValue == expectedSAvgValue);
-
-  REQUIRE(printData(&RateOfCharge[0],0.50,0.00,0.81) == 1);
-}
-
